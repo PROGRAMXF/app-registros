1b797eb68918164657fb0aacc5de7cdf00648603
Fernando <fh1273509@gmail.com>
2023-03-15 22:46:05 -0300

    configuracion src/rutas

---
+++ src/index.js
@@ -0,0 +1,19 @@
+//inicializacion
+const app = express();
+
+//settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join (__dirname, 'views'));
+app.set('views engine', 'ejs');
+
+//middleares
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//rutas
+app.use(require('./rutas/index'));
+
+//start server
+app.listen(app.get('port'), () => {
+    console.log("Server escuchando")});
+app.get('port');
